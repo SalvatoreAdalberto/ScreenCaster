035ffe7236d69d5669da97501f91b3bf98b09cb8
MARCO DEL CORE s322783 <s322783@studenti.polito.it>
2025-01-11 23:51:19 +0100

    add comments for hotkey manager

--- screen_caster/src/hotkeys.rs
+++ screen_caster/src/hotkeys.rs
@@ -12,6 +12,16 @@ use crate::streaming_server;
 use crate::gui::ShareMode;
 use iced::{ Subscription, time as iced_time};
 
+/// This module handles the global hotkeys for the application.
+/// The hotkeys are used to start and stop the screen sharing, to clear the annotations and to end a session.
+
+
+/// Enumerates the possible messages that can be sent by the hotkeys.
+/// - Start: message to start the screen sharing
+/// - Stop: message to stop the screen sharing
+/// - CloseSessionServer: message to close the session on the server side
+/// - CloseSessionClient: message to close the session on the client side
+/// The messages are used to communicate with the main application.
 #[derive(Debug, Clone)]
 pub enum HotkeyMessage {
     Start,
@@ -21,12 +31,19 @@ pub enum HotkeyMessage {
 }
 
 pub struct AppState {
-    pub(crate) is_sharing: bool, // Indica se siamo nella schermata di condivisione
-    pub(crate) streaming_server: Option<streaming_server::StreamingServer>, // Oggetto per la gestione della registrazione
-    pub(crate) share_mode: ShareMode, // Modalità di condivisione
-    pub(crate) screen_index: usize, // Indice dello schermo da condividere
-    pub(crate) annotation_stdin: Option<std::process::ChildStdin>, // Stdin per l'invio delle annotazioni
+    // Flag to check if the application is in casting mode, readay to start the screen sharing.
+    pub(crate) is_sharing: bool,
+    // The streaming server used to record the screen.
+    pub(crate) streaming_server: Option<streaming_server::StreamingServer>,
+    // The mode of sharing (Fullscreen or Area). Default is Fullscreen.
+    pub(crate) share_mode: ShareMode,
+    // The index of the screen to share. Default is 1 (the main screen).
+    pub(crate) screen_index: usize, 
+    // The stdin of the annotation tool process to send the clear command.
+    pub(crate) annotation_stdin: Option<std::process::ChildStdin>,
+    // Flag to check if the screen sharing has started.
     pub(crate) cast_started: bool,
+    // Flag to check if the session has been closed.
     pub(crate) session_closed: bool,
 }
 
@@ -43,23 +60,23 @@ impl AppState {
         }
     }
 
+    // Start the StreamingServer with the selected screen index and share mode.
     pub fn start(&mut self) {
         if self.is_sharing && !self.cast_started {
             self.session_closed = false;
             if self.streaming_server.is_none(){
                 self.streaming_server = Some(streaming_server::StreamingServer::new());
             }
-            self.streaming_server.as_mut().unwrap().start(self.screen_index, self.share_mode); // Avvia la registrazione
+            self.streaming_server.as_mut().unwrap().start(self.screen_index, self.share_mode);
             self.cast_started = true;
-        } else {
-            println!("Non siamo nella schermata di condivisione.");
         }
     }
 
+    // Stop the StreamingServer and close the annotation tool.
     pub fn stop(&mut self) {
         if self.is_sharing && self.cast_started {
             if self.streaming_server.is_some(){
-                self.streaming_server.as_mut().unwrap().stop(); // Ferma la registrazione
+                self.streaming_server.as_mut().unwrap().stop();
                 self.streaming_server = None;
             }
             self.close_annotation();
@@ -67,40 +84,41 @@ impl AppState {
         }
     }
 
+    // Send a clear command to the annotation tool to delete all the shapes.
     pub fn clear(&mut self) {
         if let Some(ref mut std) = self.annotation_stdin {
             if writeln!(std, "clear").is_ok() {
-                println!("Annotation cleared");
+                // Do nothing, the annotation tool will clear the shapes.
             } else {
-                eprintln!("Lo stdin è chiuso.");
                 self.annotation_stdin = None;
             }
-        } else {
-            eprintln!("Lo stdin non è disponibile.");
         }
     }
 
+    // Update the stdin of the annotation tool process in case it is closed and reopened.
     pub fn update_stdin(&mut self, stdin: std::process::ChildStdin) {
+        // Double check if the annotation tool is still open.
         self.close_annotation();
         self.annotation_stdin = Some(stdin);
     }
 
+    // Close the stdin of the annotation tool process.
     pub fn close_annotation(&mut self) {
         if let Some(ref mut std) = self.annotation_stdin {
+            // Send the quit command to the annotation tool.
             if writeln!(std, "quit").is_ok() {
-                println!("Annotation closed");
                 self.annotation_stdin = None;
             } else {
-                eprintln!("Lo stdin è chiuso.");
+                // The annotation tool is already closed.
                 self.annotation_stdin = None;
             }
-        } else {
-            eprintln!("Lo stdin non è disponibile.");
         }
     }
 
+    // Check if the annotation tool is still open to avoid opening multiple instances.
     pub fn check_annotation_open(&mut self) -> bool {
         if let Some(ref mut std) = self.annotation_stdin {
+            // Check if the stdin is still open.
             if writeln!(std, "check").is_ok() {
                 true
             } else {
@@ -111,6 +129,7 @@ impl AppState {
         }
     }
 
+    // Stop the session. If used by the server, it will close the StreamingServer and the annotation tool.
     pub fn stop_session(&mut self) {
         self.session_closed = true;
         if self.cast_started{
@@ -118,6 +137,8 @@ impl AppState {
         }
     }
 
+    /// Subscription to send hotkey messages to the main application.
+    /// It allows to update the gui and the application state.
     pub fn subscription(&mut self) -> Subscription<HotkeyMessage> {
         if self.cast_started && self.is_sharing {
             iced_time::every(Duration::from_secs_f32(0.1))
@@ -139,6 +160,7 @@ impl AppState {
     }
 }
 
+// Event loop for the hotkeys on MacOS.
 #[cfg(target_os = "macos")]
 pub fn macos_event_loop(id1: Arc<Mutex<u32>>, id2: Arc<Mutex<u32>>, id3: Arc<Mutex<u32>>, id4: Arc<Mutex<u32>>, app_state: Arc<Mutex<AppState>>, running: Arc<Mutex<bool>>) {
     loop {
@@ -163,6 +185,7 @@ pub fn macos_event_loop(id1: Arc<Mutex<u32>>, id2: Arc<Mutex<u32>>, id3: Arc<Mut
     }
 }
 
+// Event loop for the hotkeys on Linux.
 #[cfg(target_os = "linux")]
 pub fn linux_event_loop(id1: Arc<Mutex<u32>>, id2: Arc<Mutex<u32>>, id3: Arc<Mutex<u32>>, id4: Arc<Mutex<u32>>, app_state: Arc<Mutex<AppState>>, running: Arc<Mutex<bool>>) {
     loop {
@@ -187,6 +210,7 @@ pub fn linux_event_loop(id1: Arc<Mutex<u32>>, id2: Arc<Mutex<u32>>, id3: Arc<Mut
     }
 }
 
+// Event loop for the hotkeys on Windows.
 #[cfg(target_os = "windows")]
 pub fn windows_event_loop(id1: Arc<Mutex<u32>>, id2: Arc<Mutex<u32>>, id3: Arc<Mutex<u32>>, id4: Arc<Mutex<u32>>, app_state: Arc<Mutex<AppState>>, running: Arc<Mutex<bool>>) {
     unsafe {
@@ -231,6 +255,7 @@ pub fn windows_event_loop(id1: Arc<Mutex<u32>>, id2: Arc<Mutex<u32>>, id3: Arc<M
     }
 }
 
+// Parse the key code to the corresponding hotkey code.
 pub fn parse_key_code(key: &str) -> Option<Code> {
     match key.to_uppercase().as_str() {
         "A" => Some(Code::KeyA),
